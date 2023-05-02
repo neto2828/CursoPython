4a6e506088e77635dd4334ed292f5599a6f8da04
Néstor Miranda <nemv.1311@gmail.com>
2023-05-02 00:55:50 -0600

    empieza el dia 3

---
+++ Dia 3/1.Index.py
@@ -0,0 +1,14 @@
+#variables a usar
+mi_texto = "Esta es una prueba"
+
+#indice a encontrar
+resultado = mi_texto[-4]
+print(resultado)
+
+#metodo para encontrar un caracter
+resultado2 = mi_texto.index("n")
+print(resultado2)
+
+#metodo rindex busca de derecha a izquierda
+resultado3 = mi_texto.rindex("a")
+print(resultado3)
